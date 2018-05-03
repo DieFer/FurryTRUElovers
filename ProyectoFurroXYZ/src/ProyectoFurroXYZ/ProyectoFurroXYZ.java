776b4f8e1b41822bf3d4f731e710c80743cc7e5f
FernandoHueros <38525278+FernandoHueros@users.noreply.github.com>
2018-05-03 10:25:02 +0200

    Adios crack 19/04/201985674237

--- ProyectoFurroXYZ/src/ProyectoFurroXYZ/ProyectoFurroXYZ.java
+++ ProyectoFurroXYZ/src/ProyectoFurroXYZ/ProyectoFurroXYZ.java
@@ -17,5 +17,6 @@ public class ProyectoFurroXYZ {
    public static void main(String[] args) {
          System.out.println("Vengalidercrackmastodontevikingoheroeliderlincepecholoboespaldaplateadacampeonfenomenochampiontiranosaurio" );
         System.out.println("FIERA" );
+         System.out.println("v2" );
     }
 }
